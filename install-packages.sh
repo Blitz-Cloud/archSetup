89e436387c2070cffb2685e4a89745a1e965cf35
Blitz-Cloud <ionutp626@gmail.com>
2025-10-22 20:49:23 +0300

    Update added  nvim and grub-btrfs to the package list

--- install-packages.sh
+++ install-packages.sh
@@ -4,18 +4,23 @@ echo "Welcome to autonomous arch install"
 
 echo "Installing YAY"
 
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
+sudo pacman -S --needed  base-devel
+git clone https://aur.archlinux.org/yay.git $HOME/yay
+cd $HOME/yay
 makepkg -si
 
 echo "Installing hyprland"
 sudo pacman -S hyprland
 
+echo "Create a snapshot"
+sudo snapper create --description "Yay and hyprland are installed" 
+
+
 echo "Installing predefined packages using yay"
-cd ~/archSetup
-cat ./packages.txt | xargs -o yay -S
+cd $HOME/archSetup
+cat $HOME/archSetup/packages.txt | xargs -o yay -S
 
+sudo systemctl start grub-btrfsd
+sudo systemctl enable grub-btrfsd
 
-
-
+sudo grub-mkconfig -o /boot/grub/grub.cfg

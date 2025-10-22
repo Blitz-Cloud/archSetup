ed43906cad6535e2fb6876f18c6fdee91c674516
Blitz-Cloud <ionutp626@gmail.com>
2025-10-22 06:37:32 +0300

    Changes not tested, added some packages with a basic bash script to install them

---
+++ install-packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Welcome to autonomous arch install"
+
+echo "Installing YAY"
+
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+echo "Installing hyprland"
+sudo pacman -S hyprland
+
+echo "Installing predefined packages using yay"
+cd ~/archSetup
+cat ./packages.txt | xargs yay -S
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+1. Prima comnanda de introdus
+```bash
+sudo pacman -S git 
+git clone https://github.com/blitz-cloud/archSetup
+```
